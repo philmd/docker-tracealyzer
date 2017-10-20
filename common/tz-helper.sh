051254025f07cc1c3c15aa3edfc9d2e6f6d58b04
Philippe Mathieu-Daudé <f4bug@amsat.org>
2017-10-20 12:55:14 -0300

    let the helper be more generic

--- common/tz-helper.sh
+++ common/tz-helper.sh
@@ -1,15 +1,17 @@
 #! /bin/sh
 
-test -n "${DISPLAY}" && test -n "${XAUTHORITY}" && mono /opt/TzForFreeRTOS-3.1.3/TzForFreeRTOS.exe
+test -n "${DISPLAY}" && test -n "${XAUTHORITY}" && ${MONO:-mono} /opt/TzForFreeRTOS-${TRACEALIZER_VERSION}/TzForFreeRTOS.exe; exit $?
 
-cat << 'EOF'
+REPO_TAG="${REPO_NAME:-philmd/tracealyzer}:${TRACEALIZER_VERSION}"
+
+sed "s@REPO_TAG@${REPO_TAG}@g" << 'EOF'
 please run:
 
-docker run --rm -u $(id -u) -v /tmp:/tmp -e DISPLAY=$DISPLAY -e XAUTHORITY=/tmp/.Xauthority -v ${XAUTHORITY}:/tmp/.Xauthority -e HOME=$HOME -v $HOME/.config/"Tracealyzer for FreeRTOS Data":$HOME/.config/"Tracealyzer for FreeRTOS Data" philmd/tracealyzer:3.1.3
+docker run --rm -u $(id -u) -v /tmp:/tmp -e DISPLAY=$DISPLAY -e XAUTHORITY=/tmp/.Xauthority -v ${XAUTHORITY}:/tmp/.Xauthority -e HOME=$HOME -v $HOME/.config/"Tracealyzer for FreeRTOS Data":$HOME/.config/"Tracealyzer for FreeRTOS Data" REPO_TAG
 
 HINT: add an alias in your ~/.bashrc:
 
-alias tracealyzer='docker run --rm -u $(id -u) -v /tmp:/tmp -e DISPLAY=$DISPLAY -e XAUTHORITY=/tmp/.Xauthority -v ${XAUTHORITY}:/tmp/.Xauthority -e HOME=$HOME -v $HOME/.config/"Tracealyzer for FreeRTOS Data":$HOME/.config/"Tracealyzer for FreeRTOS Data" philmd/tracealyzer:3.1.3'
+alias tracealyzer='docker run --rm -u $(id -u) -v /tmp:/tmp -e DISPLAY=$DISPLAY -e XAUTHORITY=/tmp/.Xauthority -v ${XAUTHORITY}:/tmp/.Xauthority -e HOME=$HOME -v $HOME/.config/"Tracealyzer for FreeRTOS Data":$HOME/.config/"Tracealyzer for FreeRTOS Data" REPO_TAG'
 
 EOF
 
